2343f240a7ceeee35ff4c575fbf55e5dbde184c0
Elsa Castillo Franco <elsacastillofranco@Air-de-Elsa.home>
2022-09-05 23:05:11 +0200

    handle user auth status. Created stores.. Created AuthView. Clean app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Esta es la vista de Home</h1>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'pinia';
+import taskStore from '@/store/task';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  computed: {
+    ...mapState(taskStore, ['tasks']),
+  },
+  methods: {
+    ...mapActions(taskStore, ['fetchTasks']),
+  },
+  created() {
+    this.fetchTasks();
   },
 };
 </script>
